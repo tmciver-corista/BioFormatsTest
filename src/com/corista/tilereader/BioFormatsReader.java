376b4b61ec8e9f81912cb0a9592292a23bda04dd
Tim McIver <tim.mciver@corista.com>
2013-09-25 10:18:01 -0400

    Renamed package com.corista.bioformats.test to com.corista.tilereader. Also renamed bioformats-test.properties to tilereader.properties.

--- src/com/corista/tilereader/BioFormatsReader.java
+++ src/com/corista/tilereader/BioFormatsReader.java
@@ -1,4 +1,4 @@
-package com.corista.bioformats.test;
+package com.corista.tilereader;
 
 import ij.ImagePlus;
 
--- src/com/corista/tilereader/TileReader.java
+++ src/com/corista/tilereader/TileReader.java
@@ -1,4 +1,4 @@
-package com.corista.bioformats.test;
+package com.corista.tilereader;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
